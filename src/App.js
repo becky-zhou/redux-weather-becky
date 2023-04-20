9b9babccf84eed4adb9b1d1772fb5ddb2e44136b
Becky <tug76312@temple.edu>
2023-04-19 22:36:10 -0400

    home page form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'; 
 
-function App() {
+
+const Weather = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1>Weather Redux</h1>
+      <Form>
 
-export default App;
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label></Form.Label>
+        <Form.Control type="text" placeholder="Enter a city" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    
+
+    </>
+  )
+}
+export default Weather; 
