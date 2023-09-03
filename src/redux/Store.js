6100b5c3773c1a0bf12c5e4cb4017357f8e0b98b
Becky Zhou <117292555+becky-zhou@users.noreply.github.com>
2023-09-03 14:10:19 -0400

    Sep 3rd before deploy

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,24 +1,23 @@
 // import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-// import WeatherReducer from './Reducer'; 
+// import WeatherReducer from './Reducer';
 // import { composeWithDevTools } from "redux-devtools-extension";
 // import thunk from "redux-thunk";
 
-
 // //higher order function, takes a function as an argument
 // const WeatherStore = configureStore(WeatherReducer,composeWithDevTools(applyMiddleware(thunk))
-// ); 
+// );
 
-// export default WeatherStore;  
+// export default WeatherStore;
 
-import { configureStore } from '@reduxjs/toolkit';
-import WeatherReducer from './Reducer';
-import thunk from 'redux-thunk';
+import { configureStore } from "@reduxjs/toolkit";
+import WeatherReducer from "./Reducer";
+import thunk from "redux-thunk";
 
 const WeatherStore = configureStore({
   reducer: {
     weather: WeatherReducer,
   },
   middleware: [thunk],
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
 });
 export default WeatherStore;
